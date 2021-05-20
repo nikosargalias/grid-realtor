e2e2b81de18fd36da2721c9646e84cb94bcd4061
Nikolaos P Argalias <nikosgalias@gmail.com>
2021-05-20 01:32:45 +0100

    completed footer

---
+++ webpack.common.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: { index: path.resolve(__dirname, "src", "index.js") },
+  output: {
+    filename: "[name].[contenthash].js", 
+    path: path.resolve(__dirname, "dist"),
+    clean: true, 
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html"), 
+      filename: "index.html",
+      // excludeChunks: ['entryPoint']
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset",
+        generator: {
+          filename: "assets/imgs/[hash][ext][query]",
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/fonts/[hash][ext][query]",
+        },
+      },
+    ],
+  },
+};
